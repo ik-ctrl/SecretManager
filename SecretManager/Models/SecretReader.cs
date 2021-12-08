e7e631655a22001b469a908f3060baf2c1e26ef0
KondratievIE <kondratyev.ie@gmail.com>
2021-12-08 16:23:47 +0300

    Сделал библиотеку с записью шифрованных файлов в appdata

---
+++ SecretManager/Models/SecretReader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+
+namespace SecretManager.Models
+{
+    internal class SecretReader
+    {
+        public T Get<T>(string fullPath) where T : class, new()
+        {
+            var lockObject = new object();
+            string crypto;
+            lock (lockObject)
+            {
+                if (!Directory.Exists(Path.GetDirectoryName(fullPath)))
+                    Directory.CreateDirectory(Path.GetDirectoryName(fullPath));
+
+                using (var reader = new StreamReader(fullPath, Encoding.Default))
+                {
+                    crypto = reader.ReadToEnd();
+                }
+            }
+            var encodeString = Crypto.Decrypt(crypto);
+            return JsonSerializer.Deserialize<T>(encodeString);
+        }
+    }
+}
---
+++ SecretManager/Models/SecretWriter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+
+namespace SecretManager.Models
+{
+    internal class SecretWriter
+    {
+        public void Set<T>(string path,string cipherKey, T data) where T : class, new()
+        {
+            var lockObject = new object();
+            var fullPath = path;
+            var text = JsonSerializer.Serialize(data);
+            var encryptText=Crypto.Encrypt(text,cipherKey);
+            // var plainTextBytes = Encoding.UTF8.GetBytes(text);
+            // var save = Convert.ToBase64String(plainTextBytes);
+            lock (lockObject)
+            {
+                if (!Directory.Exists(Path.GetDirectoryName(fullPath)))
+                    Directory.CreateDirectory(Path.GetDirectoryName(fullPath));
+                
+                using (var writer = new StreamWriter(fullPath, false, Encoding.Default))
+                {
+                    //writer.WriteLine(save);
+                    writer.WriteLine(encryptText);
+                }
+            }
+        }
+    }
+}
