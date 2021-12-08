f3c373b554fdd936a13767066c006a525ac1fd9d
KondratievIE <kondratyev.ie@gmail.com>
2021-12-08 16:41:08 +0300

    Добавил способ использования

---
+++ Example/ImportantSecrets.cs
@@ -0,0 +1,21 @@
+﻿namespace Example
+{
+    class ImportantSecrets
+    {
+        public string Secret1 { get; set; } = "default";
+        public string Secret2 { get; set; } = "default";
+
+        public string Secret3 { get; set; } = "default";
+        public string Secret4 { get; set; } = "default";
+
+        public string Secret5 { get; set; } = "default";
+        public string Secret6 { get; set; } = "default";
+
+        public int Secret7 { get; set; } = 1;
+        public uint Secret8 { get; set; } = 1;
+
+        public double Secret9 { get; set; } = 1;
+        public decimal Secret10 { get; set; } = 1;
+
+    }
+}
