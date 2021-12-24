3d9b0c41acded16e1b72d01eb7fedff7564a9608
KondratievIE <kondratyev.ie@gmail.com>
2021-12-24 10:56:00 +0300

    отрефакторил пример

--- Example/Program.cs
+++ Example/Program.cs
@@ -18,12 +18,9 @@ namespace Example
             Console.WriteLine($"Записываем=>{JsonSerializer.Serialize(secret)}");
             var result = SecretManaged.Set(secret);
             Console.WriteLine($"Результат операции записи:{result}");
-            
             var returnSecret = SecretManaged.Get<ImportantSecrets>();
             Console.WriteLine($"Возвращаемое значение=>{JsonSerializer.Serialize(returnSecret)}");
-            
             Console.WriteLine($"Результат удаления секрета:{SecretManaged.DeleteSecret()}");
-
             var emptySecret = SecretManaged.Get<ImportantSecrets>();
             Console.WriteLine($"Считывание серета если файла нет (значение по умолчанию)=>{JsonSerializer.Serialize(emptySecret)}");
             Console.WriteLine("~Конец~");
